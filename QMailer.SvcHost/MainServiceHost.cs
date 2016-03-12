f9bcb6c4d02ae609aa72b2f6516d01f3c64856e1
Marc Chouteau <chouteau@gmail.com>
2016-03-12 09:44:22 +0100

    Replace inner svchost by Palace

---
+++ QMailer.SvcHost/MainServiceHost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using QMailer;
+
+namespace QMailer.SvcHost
+{
+	public partial class MainServiceHost
+	{
+		public MainServiceHost()
+		{
+		}
+
+		public void Initialize()
+		{
+		}
+
+		public void Start()
+		{
+			QMailerService.Start();
+		}
+
+		public void Stop()
+		{
+			QMailerService.Stop();
+		}
+	}
+}
